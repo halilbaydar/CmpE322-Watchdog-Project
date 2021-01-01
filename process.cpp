b73cd32e5cfb4a82847e40efd050e6798c8ca0a6
halilbaydar <halilbaydar@yahoo.com>
2021-01-01 10:44:21 +0300

    finish

--- process.cpp
+++ process.cpp
@@ -1,47 +1,65 @@
 /**
  @author Halil Baydar
+ @no 2017400297 
  @date December 2020
  */
 #include <bits/stdc++.h>
-#include <signal.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <sys/wait.h> 
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 using namespace std;
-string name="";
-string filepath="";
-fstream output;
-
-void sighandler(int sentsignal){
-    output.open(filepath,ios::app);
-    signal(sentsignal,sighandler);
-    output<< name+" received signal "<<sentsignal;
-    if(sentsignal==15){
-        output<<",terminating gracefully"<<endl;
+string name = "";     /**< this variable is to keep name of process given by watcdog process*/
+string filepath = ""; /**<this variable is for the output file path*/
+fstream output;       /**<this variable is to write output into given file path*/
+/**
+ * @see sighandler
+ * @param sentsignal this parameter is to keep sent signal number
+ * this functin handles the given signal that means write outputs according to given signal
+ * if the sent signal is SIGTERM, process is quit with exit(0)
+*/
+void sighandler(int sentsignal)
+{
+    output.open(filepath, ios::app);
+    signal(sentsignal, sighandler);
+    output << name + " received signal " << sentsignal;
+    if (sentsignal == 15)
+    {
+        output << ", terminating gracefully" << endl;
         output.close();
         exit(0);
     }
-    output<<endl;
+    output << endl;
     output.close();
 }
-int main(int argc , char * argv []){
-    name="P";
-    name+=(char)*argv[2];
-    filepath=argv[1];
-    output.open(argv[1],ios::app);
-    output <<name<<" is waiting for a signal\n";
+/**
+ * @see main
+ * @param argc this variable is for given parameters total size
+ * @param argv this variable is used to take given parameters
+ * main function continues until SOIGTERM signal is sent thanks to infinite for loop
+*/
+int main(int argc, char *argv[])
+{
+    name = "P";
+    for(int i=0; i<strlen(argv[2]); i++)
+        name +=argv[2][i];
+    filepath = argv[1];
+    output.open(argv[1], ios::app);
+    output << name << " is waiting for a signal\n";
     output.close();
-    signal(SIGHUP,sighandler);
-    signal(SIGINT,sighandler);    
-    signal(SIGILL,sighandler);
-    signal(SIGTRAP,sighandler);
-    signal(SIGFPE,sighandler);
-    signal(SIGBUS,sighandler);
-    signal(SIGSEGV,sighandler);
-    signal(SIGTERM,sighandler);
-    signal(SIGXCPU,sighandler);
-    for(;;){}
+    signal(SIGHUP, sighandler);
+    signal(SIGINT, sighandler);
+    signal(SIGILL, sighandler);
+    signal(SIGTRAP, sighandler);
+    signal(SIGFPE, sighandler);
+    signal(SIGBUS, sighandler);
+    signal(SIGSEGV, sighandler);
+    signal(SIGTERM, sighandler);
+    signal(SIGXCPU, sighandler);
+    for (;;)
+    {
+    }
     return 0;
 }
--- watchdog.cpp
+++ watchdog.cpp
@@ -1,11 +1,12 @@
 /**
  @author Halil Baydar
+ @no 2017400297
  @date December 2020
  */
 
 /*
    The watchdog.cpp keeps track af all child processes and checks whether a child is died 
-   abnormaly or not. If it is, watchdog creates the child again
+   or not. If it is, watchdog creates the child again
 */
 #include <bits/stdc++.h>
 #include <sys/types.h>
@@ -22,26 +23,49 @@
 #include <sstream>
 #include <fstream>
 #include <map>
-struct timespec delta = {0 /*secs*/, 300000000 /*nanosecs*/}; //0.5 sec.
-struct timespec deltaforexec = {0 /*secs*/, 200000000 /*nanosecs*/}; //0.5 sec
+#include <functional>
+struct timespec delta = {0 /*secs*/, 300000000 /*nanosecs*/}; /**<This time is taken from executor file to keep order 
+of child process outputs*/
 using namespace std;
-void sigterm(int sigterm);
-ofstream outf;
-map<int, string> childids;
-int unnamedPipe;
+/**
+ * @see sigterm
+*/
+void sigterm(int segterm);           /**<This function is SIGTERM signal handling*/
+ofstream outf;                       /**<this variable is to give the output of watchdog process to given file as parameter*/
+map<int, string> childids;           /**<this map keep child ids and names in that manner to keep track of the childeren*/
+vector<pair<string, int>> childids2; /**<this is an also keep names and ids of childeren but in this map the order 
+of child is not changed after any child is gone. Thanks to this map I kill all process in ascending order*/
+int unnamedPipe;                     /**< this variable is for unnamedpipe commutication with executor process*/
+int numberofprocess;                 /**<This varibale keeps the number of child processes*/
+string process_output;               /**<This variable keeps the process output file path in string type*/
+string watchdog_output;              /**<this variable keeps the watchdog output file path in string type*/
+
+pid_t result; /**<this variable keeps the dead child id*/
+char *myfifo; /*<This is for common file to used between watchdog and executor*/
+/**
+ * @see main
+ * The main function gets the parameters and according to given the number of children 
+ * create child process and gives them their parameter which is output path and the child id like "P1"
+ * Child process is converted into prcess.cpp binary code by taking parameters process output file path 
+ * and name
+ * while loop continues until all instructions are executed
+ * this while loop waits until any child is gone
+ * if gone process is "P1" all process is terminated and again created, if not, only related child is 
+ * again created.
+ * @param argc gives the size of the given parametes
+ * @param argv gives the parameter to me: the number of process, process output file, wathcdog output file
+ * */
 int main(int argc, char *argv[])
 {
-    int numberofprocess;
     numberofprocess = atoi(argv[1]);
-    string process_output = argv[2];
-    string watchdog_output = argv[3];
-    outf.open(watchdog_output);
+    process_output = argv[2];
+    watchdog_output = argv[3];
     pid_t pid;
     int i;
     for (i = 0; i < numberofprocess; i++)
     {
         pid = fork();
-        if (pid == 0) /* creating a child process */
+        if (pid == 0)
         {
             execl("./process", "./process", process_output.c_str(), to_string(i + 1).c_str(), NULL);
             cout << "fail";
@@ -50,49 +74,55 @@ int main(int argc, char *argv[])
         {
             sleep(1);
             childids[pid] = "P" + to_string(i + 1);
+            childids2.push_back({"P" + to_string(i + 1),pid});
             continue;
         }
         else if (pid < 0)
         {
-            outf << "fail";
-            return 1;
+            i--;
+            continue;
         }
     }
 
     char *myfifo = (char *)"/tmp/myfifo";
     unnamedPipe = open(myfifo, O_WRONLY);
-
+    outf.open(watchdog_output);
     string s = "P0 ";
     s += to_string(getpid());
     char watchdogid[s.size()];
     strcpy(watchdogid, s.c_str());
-    write(unnamedPipe, watchdogid, 30);
+    write(unnamedPipe, watchdogid, 30); /**/
 
     for (map<int, string>::iterator it = childids.begin(); it != childids.end(); it++)
     {
         string tmp = it->second;
         tmp += " ";
         tmp += to_string(it->first);
+        outf << it->second << " "
+             << "is started and it has a pid of " << it->first << '\n';
         write(unnamedPipe, tmp.c_str(), 30);
     }
+    signal(SIGTERM, sigterm);
     while (true)
     {
-        pid_t result;
         result = wait(NULL);
+        if(result==-1)
+            continue;
         if (childids[result] == "P1")
         {
             outf << "P1 is killed, all processes must be killed\n";
             nanosleep(&delta, &delta);
             outf << "Restarting all processes\n";
-            map<int, string>::iterator it;
-            it = childids.begin();
-            for (it++; it != childids.end(); it++)
+            vector<pair<string, int>>::iterator it;
+            it = childids2.begin();
+            for (it++; it != childids2.end(); it++)
             {
-                kill(it->first, 15);
+                kill(it->second, 15);
                 nanosleep(&delta, &delta);
                 wait(NULL);
             }
             childids.clear();
+            childids2.clear();
             pid_t pid = 0;
             int i;
             for (i = 0; i < numberofprocess; i++)
@@ -105,7 +135,8 @@ int main(int argc, char *argv[])
                 {
                     sleep(1);
                     childids[pid] = "P" + to_string(i + 1);
-                    outf << "P" + to_string(i + 1) << " is started and it has a pid of" << pid << "\n";
+                    childids2.push_back({"P" + to_string(i), pid});
+                    outf << "P" + to_string(i + 1) << " is started and it has a pid of " << pid << "\n";
                 }
                 else if (pid < 0)
                 {
@@ -132,28 +163,41 @@ int main(int argc, char *argv[])
             {
                 execl("./process", "./process", process_output.c_str(), id.c_str(), NULL);
             }
-            childids[newchild] = childids.at(result);
-            string tmp = childids.at(newchild) + " ";
+            string tmp = childids.at(result) + " ";
             tmp += to_string(newchild);
             write(unnamedPipe, tmp.c_str(), 30);
+            childids[newchild] = childids.at(result);
             childids.erase(result);
+            for (int i = 0; i < numberofprocess; i++)
+            {
+                if (childids2[i].second == result)
+                {
+                    childids2[i].second = newchild;
+                    break;
+                }
+            }
             outf << childids.at(newchild) << " is started and it has a pid of " << newchild << endl;
         }
     }
-    signal(SIGTERM, sigterm);
     return 0;
 }
+/**
+ * the sigterm function is called if the watchdog gets a SIGTERM signal from executor
+ * @param segterm says us the given signal is 15
+ * */
 void sigterm(int segterm)
 {
     close(unnamedPipe);
-    outf << "Watchdog is terminating gracefully";
-    outf.close();
-    map<int, string>::iterator it;
-    for (it = childids.begin(); it != childids.end(); it++)
+    cout << "Watchdog is terminating gracefully";
+    vector<pair<string, int>>::iterator it;
+    for (it = childids2.begin(); it != childids2.end(); it++)
     {
-        kill(it->first, 15);
+        kill(it->second, 15);
         nanosleep(&delta, &delta);
-        //wait(NULL);
+        wait(NULL);
     }
-    exit(0);
+    if(segterm==15){
+        outf.close();
+        exit(0);
+    }
 }
